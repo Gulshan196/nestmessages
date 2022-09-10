2b91cfdd0ffaa130c2b012eeac153a51811a0c2d
Gulshan196 <109276430+Gulshan196@users.noreply.github.com>
2022-09-10 16:35:02 +0530

    first commit

---
+++ src/messages/messages.repository.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import { readFile, writeFile } from 'fs/promises';
+
+@Injectable()
+export class MessagesRepository {
+  async findOne(id: string) {
+    const contents = await readFile('messages.json', 'utf8');
+    const messages = JSON.parse(contents);
+
+    return messages[id];
+  }
+
+  async findAll() {
+    const contents = await readFile('messages.json', 'utf8');
+    const messages = JSON.parse(contents);
+
+    return messages;
+  }
+
+  async create(content: string) {
+    const contents = await readFile('messages.json', 'utf8');
+    const messages = JSON.parse(contents);
+
+    if(messages){
+      const id = Math.floor(Math.random() * 999);
+
+      messages.push({ id, content });
+    }
+
+    
+
+    await writeFile('messages.json', JSON.stringify(messages));
+  }
+}
